f33f695399cfffe00dbfff5fe8e5252d7846a3e9
Nicholas Lepage <nic.lep@hotmail.com>
2018-06-28 22:33:19 +0000

    Added Login and Sign Up modal that pops up

--- public/scripts/animate.js
+++ public/scripts/animate.js
@@ -1,9 +1,19 @@
 $(document).ready(function() {
 
-// console.log("Ready");
-//   $( ".chickin" ).on("mouseover", function() {
-//     console.log("Im here");
-//       $(".chickin").effect("bounce", 1000);
-//   });
+const $login = $('.loginButton');
+$login.on('click', function () {
+  $('.bg-modal-login').css({display: 'flex'});
+});
+
+const $signup = $('.signupButton');
+$signup.on('click', function () {
+  $('.bg-modal-signup').css({display: 'flex'});
+});
+
+const $close = $('.close');
+$close.on('click', function () {
+  $('.bg-modal-login').css({display: 'none'});
+  $('.bg-modal-signup').css({display: 'none'});
+});
 
 });
--- server.js
+++ server.js
@@ -2,20 +2,24 @@
 
 require('dotenv').config();
 
-const PORT        = process.env.PORT || 8080;
-const ENV         = process.env.ENV || "development";
-const express     = require("express");
-const bodyParser  = require("body-parser");
-const sass        = require("node-sass-middleware");
-const app         = express();
+const PORT          = process.env.PORT || 8080;
+const ENV           = process.env.ENV || "development";
+const express       = require("express");
+const bodyParser    = require("body-parser");
+const sass          = require("node-sass-middleware");
+const app           = express();
+const cookieParser  = require('cookie-parser');
+const cookieSession = require('cookie-session');
+const bcrypt        = require('bcryptjs');
 
-const knexConfig  = require("./knexfile");
-const knex        = require("knex")(knexConfig[ENV]);
-const morgan      = require('morgan');
-const knexLogger  = require('knex-logger');
+
+const knexConfig    = require("./knexfile");
+const knex          = require("knex")(knexConfig[ENV]);
+const morgan        = require('morgan');
+const knexLogger    = require('knex-logger');
 
 // Seperated Routes for each Resource
-const usersRoutes = require("./routes/users");
+const usersRoutes   = require("./routes/users");
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
@@ -24,6 +28,13 @@ app.use(morgan('dev'));
 
 // Log knex SQL queries to STDOUT as well
 app.use(knexLogger(knex));
+
+app.use(cookieSession({
+  name: 'session',
+  keys: ["big boi with big secrets"],
+
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}));
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
