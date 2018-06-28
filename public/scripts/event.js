8e002b7fb701a044645564e471c978eea488490c
Eliza Moore <elzmoore13@gmail.com>
2018-06-28 17:03:00 +0000

    styling updates - fixes after crash...

--- public/scripts/event.js
+++ public/scripts/event.js
@@ -1,10 +1,19 @@
-// $(() => {
-//   $.ajax({
-//     method: "GET",
-//     url: "/api/event"
-//   }).done((users) => {
-//     for(user of users) {
-//       $("<div>").text(user.name).appendTo($("body"));
-//     }
-//   });;
-// });
+
+$(document).ready(function() {
+  console.log("TEST")
+
+  const $selection = $('.btn-choice')
+
+  $selection.on('click', () => {
+    console.log("HERE");
+    const $tableSpot = $($selection.parent());
+    //console.log($tableSpot)
+    $tableSpot.addClass('check');
+    const $check = $(`<i class="fas fa-check-circle fa-2x"></i>`)
+    $check.appendTo('$tableSpot')
+
+    $selection.remove();
+
+  });
+
+});
