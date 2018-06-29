a15704afeb451747a4ab75de8b97d5f7d3836d50
Eliza Moore <elzmoore13@gmail.com>
2018-06-29 18:53:33 +0000

    add submission ability to event page (no DB connection yet for submission)

--- public/scripts/event.js
+++ public/scripts/event.js
@@ -6,8 +6,6 @@ $(document).ready(function() {
 
   $selection.on('click', (event) => {
     event.stopPropagation();
-    console.log("HERE");
-    console.log(event.currentTarget);
     const $check = $(`<i class="fas fa-check-circle fa-2x"></i>`)
     const $tableSpot = $(event.currentTarget);
     if ($tableSpot.hasClass('check')){
@@ -22,5 +20,74 @@ $(document).ready(function() {
     }
 
   });
+
+  const $submitChoices = $('#vote')
+  let clicked = false;
+
+  $submitChoices.on('click', (event) => {
+
+    console.log("HERE");
+    if(!clicked){
+
+    clicked = true;
+
+    const $rowToSubmit = $('.new-user .name')
+
+    const $username = $('.new-user .name .usernameInput')
+
+    const $usernameSubmission = $(`<td class='user-column'>${$username.val()}</td>`)
+
+    $rowToSubmit.replaceWith($usernameSubmission);
+
+    const $yesBlocks = $('.new-user .check')
+    $yesBlocks.parent().addClass('check')
+    $yesBlocks.replaceWith(`<i class="fas fa-check-circle fa-2x"></i>`)
+
+    const $noBlocks = $('.new-user .btn-choice')
+    $noBlocks.parent().addClass('ex')
+    $noBlocks.replaceWith(`<i class="far fa-times-circle fa-2x"></i>`)
+
+  } else {
+
+    clicked = false
+
+
+
+    // $('.new-user').replaceWith(`<tr class='new-user'>
+    //   <td class='name'>
+    //     <form>
+    //       <input class='usernameInput' type='username' placeholder='Your name'/>
+    //       <input class='emailInput'type='email' placeholder='myEmail@Nielemma.com'/>
+    //     </form>
+    //   </td>
+    //
+    //   <% for(let i = 0; i < columnCount; i++) { %>
+    //         <td><button class='btn-choice'>choose</button></td>
+    //    <% }  %>
+    //
+    // </tr>`)
+
+  }
+
+  });
+
+// <td class='check'><i class="fas fa-check-circle fa-2x"></i></td>
+//
+// <td class='ex'><i class="far fa-times-circle fa-2x"></i></td>
+
+
+  // <tr class='new-user'>
+  //   <td>
+  //     <form>
+  //       <input type='username' placeholder='Your name'/>
+  //       <input type='email' placeholder='myEmail@Nielemma.com'/>
+  //     </form>
+  //   </td>
+  //
+  //   <% for(let i = 0; i < columnCount; i++) { %>
+  //         <td><button class='btn-choice'>choose</button></td>
+  //    <% }  %>
+  //
+  // </tr>
 
 });
