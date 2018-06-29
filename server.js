4ffce9373b8d86a90e0dbe95ef1c9e94889d2697
Eliza Moore <elzmoore13@gmail.com>
2018-06-29 01:49:55 +0000

    part way through adding user's voting choices to table

--- server.js
+++ server.js
@@ -54,75 +54,110 @@ app.get("/events/:id", (req, res) => {
        dayName: [],
        month: [],
        year: [],
+       startTime: [],
+       endTime: [],
        users:[],
+       userChoices: [],
        currentUser: '',
 
      };
 
-Promise.all([
+  Promise.all([
 
-  //find how many date options there were for the selected event
-  knex.where({
-      eventId: 1,
-      })
-      .count('*')
-      .from('date_options')
-      .then(function(result) {
-
-        templateVars['columnCount'] = result[0].count;
-      }),
-
-  //get the specific dates for each option
-  knex.where({
+    //find how many date options there were for the selected event
+    knex.where({
         eventId: 1,
-      })
-      .select('*')
-      .from('date_options')
-      .then(function(result) {
-         for(let i in result){
-           let date = String(result[i].date)
-           templateVars['dayName'].push(date.slice(0,3));
-           templateVars['dayNum'].push(date.slice(8,10));
-           templateVars['month'].push(date.slice(4,7));
-           templateVars['year'].push(date.slice(11,15));
-         }
-      }),
+        })
+        .count('*')
+        .from('date_options')
+        .then(function(result) {
 
+          templateVars['columnCount'] = result[0].count;
+        }),
 
-   //get the users that have responded to the selected event
-   knex.where({
+    //get the specific dates for each option
+    knex.where({
           eventId: 1,
-       })
-       .select('*')
-       .from('users')
-       .then(function(result) {
-         // console.log(result)
-         for(let i in result){
-           let userName = result[i].name;
-           let userEmail = result[i].email;
-           templateVars['users'].push([userName, userEmail]);
-         }
-         console.log('USERS', templateVars['users'][0][0]);
-       }),
+        })
+        .select('*')
+        .from('date_options')
+        .then(function(result) {
+           for(let i in result){
+             let date = String(result[i].date)
+             templateVars['dayName'].push(date.slice(0,3)); //dayName
+             templateVars['dayNum'].push(date.slice(8,10)); //dayNum
+             templateVars['month'].push(date.slice(4,7)); //month
+             templateVars['year'].push(date.slice(11,15)); //year
+
+             let startTime = formatTime(result[i].timeStart);
+             templateVars['startTime'].push(startTime); //startTime
+
+             let endTime = formatTime(result[i].timeEnd);
+             templateVars['endTime'].push(endTime); //endTime
+
+           }
+        }),
 
 
-]).then(function(result) {
+     //get the users that have responded to the selected event
+     knex.where({
+            eventId: 1,
+         })
+         .groupBy('id')
+         .select('*')
+         .from('users')
+         .then(function(result) {
+           // console.log(result)
+           for(let i in result){
+             let userName = result[i].name;
+             let userEmail = result[i].email;
+             templateVars['users'].push([userName, userEmail, []]);
 
-  res.render("event", templateVars);
+             //get each user's selected date_options
+             knex.where({
+                    usersId: i,
+                    eventId: 1,
+                 })
+                 .select('*')
+                 .from('usersDateOptions')
+                 .innerJoin('date_options', 'usersDateOptions.dateOptionsId', 'date_options.id')
+                 .then(function(result) {
+                   console.log(result);
+                   for(let i in result){
+                     console.log('\n\ndate options', result[i].dateOptionsId)
+                     templateVars['users'][i][2].push(result[i].dateOptionsId);
+                   }
+                 });
+           }
 
-})
 
 
+         }),
 
 
-  // let templateVars = {
-  //       uniqueUrl: req.params.id,
-  //       // longURL: Document.URL
-  //       columnCount: columnCount,
-  //
-  //     };
-  // res.render("event", templateVars);
-    });
+  ]).then(function(result) {
+
+    res.render("event", templateVars);
+
+  })
+
+});
+
+
+    //helper function to put times in 12 hour clock and format
+    //
+const formatTime = function(timeObject){
+    let newTime = String(timeObject).slice(16,21);
+    let temp = Number(newTime.slice(0,2));
+    if(temp > 12){
+      temp -= 12;
+      return '0'+temp+newTime.slice(2)+'pm';
+    } else if (temp == 0){
+      return '12:00am';
+    } else{
+      return newTime+'am';
+    }
+  }
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
