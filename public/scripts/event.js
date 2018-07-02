03a8057d52c23de50d10e356088942b89f84b77d
Eliza Moore <elzmoore13@gmail.com>
2018-07-02 02:09:20 +0000

    small updates to email - still buggy

--- public/scripts/event.js
+++ public/scripts/event.js
@@ -26,9 +26,18 @@ $(document).ready(function() {
 
       $('.jumbotron').append(`<br />
                               <h4>Invite attendees with this link extension:</h4>
-                              <h4>${uniqueUrl}   <i class="far fa-copy"></i><h4><div class="fb-share-button" data-href="http://localhost:8080/events/j99nsgu06ko" data-layout="button" data-size="large" data-mobile-iframe="false"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Flocalhost%3A8080%2Fevents%2Fj99nsgu06ko&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a></div>
+                              <h4>${uniqueUrl}   <i class="far fa-copy"></i><h4>
+
+                              <form  class='emailTheLink' method='POST' action="mailto:?subject=Test?&body=Hey!%20Youve%20been%20invited%20to%20submit%20youre%20availability%20for...">
+                                <button type='submit'>Email The Link!</button><h4>
+                              </form>
+
+                              <div class="fb-share-button" data-href="http://localhost:8080/events/j99nsgu06ko" data-layout="button" data-size="large" data-mobile-iframe="false"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Flocalhost%3A8080%2Fevents%2Fj99nsgu06ko&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a></div>
 
                               `)
+
+      // $('.jumbotron').children('form').removeClass('emailTheLink')
+      // $('.jumbotron').children('form').removeClass('emailTheLink')
 
       $('.far.fa-copy').on('click', (event) => {
         copyLink(uniqueUrl);
